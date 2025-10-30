80c953e35e385b25b8490804e2b794018e6d0659
Yashodha <yashodha.anuthi@gmail.com>
2025-10-30 13:42:30 +0530

    F_final

--- dsm-application.client/src/environments/environment.ts
+++ dsm-application.client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7189/api'  // 👈 base API URL for local dev
+  apiUrl: 'https://dsm-application.onrender.com/api'  // 👈 base API URL for local dev
 };

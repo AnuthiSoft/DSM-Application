746e8dfb15b86e4d649423a611b803f06cded3af
Yashodha <yashodha.anuthi@gmail.com>
2025-11-03 11:19:10 +0530

    R_token2

--- DSM-Application.Server/Models/RefreshToken.cs
+++ DSM-Application.Server/Models/RefreshToken.cs
@@ -1,6 +1,19 @@
-﻿namespace DSM_Application.Server.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DSM_Application.Server.Models
 {
     public class RefreshToken
+
+
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsRevoked { get; set; } = false;
     }
 }

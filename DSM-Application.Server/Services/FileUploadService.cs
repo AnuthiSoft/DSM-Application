44b1dd45888f470a3726d4f11cf567c71076710f
Yashodha <yashodha.anuthi@gmail.com>
2025-11-05 13:13:29 +0530

    img_test2

---
+++ DSM-Application.Server/Services/FileUploadService.cs
@@ -0,0 +1,29 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace DSM_Application.Server.Services
+{
+    public class FileUploadService
+    {
+        private readonly Cloudinary _cloudinary;
+        public FileUploadService(IConfiguration config)
+        {
+            var account = new Account(
+                config["Cloudinary:CloudName"],
+                config["Cloudinary:ApiKey"],
+                config["Cloudinary:ApiSecret"]);
+            _cloudinary = new Cloudinary(account);
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            using var stream = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream)
+            };
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            return uploadResult.SecureUrl.AbsoluteUri;
+        }
+    }
+}
--- DSM-Application.Server/obj/Debug/net8.0/DSM-Application.Server.AssemblyInfo.cs
+++ DSM-Application.Server/obj/Debug/net8.0/DSM-Application.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSM-Application.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e077fda6052ba414b245bb347ccb34b64bfb4fba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf0a93e9b57ebff1768525e9d663bf8bc148f91")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSM-Application.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSM-Application.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

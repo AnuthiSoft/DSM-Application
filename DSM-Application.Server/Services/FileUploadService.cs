343d3b8e51cfcfb4656f208193ccbe61136953ad
Yashodha <yashodha.anuthi@gmail.com>
2025-11-06 11:41:07 +0530

    Revert "environment variable change"
    This reverts commit 8f5e267ff62d87fcb5ee9f7577ae366ad51c63cc.

--- DSM-Application.Server/Services/FileUploadService.cs
+++ DSM-Application.Server/Services/FileUploadService.cs
@@ -10,10 +10,10 @@ namespace DSM_Application.Server.Services
         public FileUploadService(IConfiguration config)
         {
             var account = new Account(
-       config["Cloudinary:CloudName"],
-       config["Cloudinary:ApiKey"],
-       config["Cloudinary:ApiSecret"]
-   );
+                config["Cloudinary:dgtavzpw1"],
+                config["Cloudinary:474935927313747"],
+                config["Cloudinary:QvHTsecBN5UOMZiuoIG0l5js8No\r\n\r\n\r\n"]
+            );
             _cloudinary = new Cloudinary(account);
             _cloudinary.Api.Secure = true; // use https
         }

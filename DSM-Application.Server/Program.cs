bd60d3496000a69d7831ca09f345754ca9d53115
Yashodha <yashodha.anuthi@gmail.com>
2025-11-17 10:11:49 +0530

    Revert "hosting_check_program.cs"
    This reverts commit e214bb3dddf00eb74a2e4633a5219809e8f0a53b.

--- DSM-Application.Server/Program.cs
+++ DSM-Application.Server/Program.cs
@@ -43,12 +43,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular", policy =>
     {
         policy.WithOrigins(
-                 "https://dsm-application.web.app",
-                 "http://localhost:58555",   
-
-            "https://dsm-application.onrender.com"
-          
-        // deployed Angular
+            "http://localhost:58555",         // local Angular dev
+            "https://dsm-application.onrender.com" // deployed Angular
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
@@ -68,8 +64,6 @@ builder.Services.AddSingleton<ProductService>();
 builder.Services.AddSingleton<EmployeeService>();
 //builder.Services.Configure<EmployeeService>(builder.Configuration.GetSection("Email"));
 builder.Services.AddSingleton<EmailService>();
-builder.Services.AddSingleton<ReviewService>();
-builder.Services.AddSingleton<FraudService>();
 
 builder.Services.AddSwaggerGen();
 
